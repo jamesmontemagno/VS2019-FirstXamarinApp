08ae86ee90c5a0c920c7786a5cd2aa2d102ba717
James Montemagno <james.montemagno@gmail.com>
2019-04-01 14:51:49 -0700

    Demo Start

--- MyFirstMobileApp/MyFirstMobileApp/App.xaml.cs
+++ MyFirstMobileApp/MyFirstMobileApp/App.xaml.cs
@@ -13,7 +13,9 @@ namespace MyFirstMobileApp
         //To debug on Android emulators run the web backend against .NET Core not IIS
         //If using other emulators besides stock Google images you may need to adjust the IP address
         public static string AzureBackendUrl =
-            DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000" : "http://localhost:5000";
+            DeviceInfo.Platform == DevicePlatform.Android 
+            ? "http://10.0.2.2:5000" 
+            : "http://localhost:5000";
 
         public static bool UseMockDataStore = false;
 
